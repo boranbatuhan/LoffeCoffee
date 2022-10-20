bd95717dd7d22038d2e56ebda6bfa72022200539
6atuhan <boranbatuh@gmail.com>
2022-10-20 21:31:43 +0300

    kart
    kart görseli tasarlayıp ekledim

--- src/router/views/CardPage.vue
+++ src/router/views/CardPage.vue
@@ -1,5 +1,100 @@
 <template>
-    <div class="w-full  flex gap-10 justify-evenly items-center  bg-[#432818]">
-            
-            </div>
+    <div class="w-full  flex gap-10 justify-evenly items-center select-none bg-[#432818]">
+        <div class="lcCard relative overflow-hidden flex flex-row yellowgold flex-wrap hover:scale-110 transition-all">
+            <img class="absolute -right-32 shrink-0 " src="/src/assets/blob.svg" alt="">
+            <img class="absolute -top-32 shrink-0 " src="/src/assets/blob-1.svg" alt="">
+            <img class="absolute -left-32 -top-10 shrink-0 " src="/src/assets/blob-2.svg" alt="">
+            <img class="absolute bottom-2 right-2 opacity-60 w-16 h-16 shrink-0 " src="/src/assets/loffee.png" alt="">
+            <p class="font-cSugar m-auto text-center w-[323px] z-20 text-9xl absolute opacity-50  leading-[200px] -rotate-[30deg] text-outline text-white"> Loffe'Coffee</p>
+        </div>        
+    </div>
 </template>
+
+
+<style>
+.lcCard{
+    height: 204px;
+    width: 323px;
+    border: 2px #22140c solid;
+    border-radius: 12px;
+background: linear-gradient(225deg, #462a1a, #3b2316);
+box-shadow:  -14px 14px 28px #22140c,
+             14px -14px 28px #603a24;
+}
+.yellowgold p {
+    text-shadow: 2px 2px 5px black;
+}
+.yellowgold {
+  color: rgba(73, 45, 21, 0.72);
+  text-decoration: none;
+  font-weight: bold;
+
+
+
+  background:
+  -moz-linear-gradient(
+    -72deg,
+    #ffc373,
+    #ffffff 16%,
+    #ffc373 21%,
+    #ffffff 24%,
+    #a17434 27%,
+    #ffc373 36%,
+    #ffffff 45%,
+    #ffffff 60%,
+    #ffc373 72%,
+    #ffffff 80%,
+    #ffc373 84%,
+    #a17434
+  );
+  background:
+  -webkit-linear-gradient(
+    -72deg,
+    #ffc373,
+    #ffffff 16%,
+    #ffc373 21%,
+    #ffffff 24%,
+    #a17434 27%,
+    #ffc373 36%,
+    #ffffff 45%,
+    #ffffff 60%,
+    #ffc373 72%,
+    #ffffff 80%,
+    #ffc373 84%,
+    #a17434
+  );
+  background:
+  -o-linear-gradient(
+    -72deg,
+    #ffc373,
+    #ffffff 16%,
+    #ffc373 21%,
+    #ffffff 24%,
+    #a17434 27%,
+    #ffc373 36%,
+    #ffffff 45%,
+    #ffffff 60%,
+    #ffc373 72%,
+    #ffffff 80%,
+    #ffc373 84%,
+    #a17434
+  );
+  background:
+  linear-gradient(
+    -72deg,
+    #ffc373,
+    #ffffff 16%,
+    #ffc373 21%,
+    #ffffff 24%,
+    #a17434 27%,
+    #ffc373 36%,
+    #ffffff 45%,
+    #ffffff 60%,
+    #ffc373 72%,
+    #ffffff 80%,
+    #ffc373 84%,
+    #a17434
+  );
+}
+
+</style>
